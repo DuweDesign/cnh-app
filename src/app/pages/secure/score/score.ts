ba91694d39e3a5cd23b39621326de392153a8ee2
Falk Duwe <info@falkduwe.de>
2026-05-11 13:06:00 +0200

    s

--- src/app/pages/secure/score/score.ts
+++ src/app/pages/secure/score/score.ts
@@ -128,8 +128,6 @@ export class Score {
 
     this.profileService.getMyProfile(dealernumber).subscribe({
       next: (profile) => {
-        console.log('this.competitionConfig()?.key: ', this.competitionConfig()?.key);
-        console.log('profile.competition: ', profile.competition);
         if (profile.competition !== this.competitionConfig()?.key) {
           throw 'Profil nicht gefunden!'
         }
